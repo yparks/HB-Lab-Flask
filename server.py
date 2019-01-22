de7da6078565183bff30b73e8875af24988b7187
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-22 15:15:59 -0800

    Flask lab finished

---
+++ server.py
@@ -0,0 +1,90 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+
+@app.route("/")
+def start_here():
+    """Home page."""
+
+    return """
+    <!doctype html>
+    <html>
+    Hi! This is the home page.
+    <a href="http://localhost:5000/hello">Link Text</a>
+    </html>
+    """
+
+
+@app.route("/hello")
+def say_hello():
+    """Say hello and prompt for user's name."""
+    options = []
+    for word in AWESOMENESS:
+
+        options.append(
+            f'<option value="{word}">{word}</option>'
+        )
+    
+    nice = "".join(options)
+    
+
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person">
+          <select name="nice">
+          {nice}
+          </select>
+          <input type="submit" value="Submit"><br>
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route("/greet")
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    # compliment = choice(AWESOMENESS)
+
+    compliment = request.args.get("nice")
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {compliment}!
+      </body>
+    </html>
+    """
+
+
+if __name__ == "__main__":
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True, host="0.0.0.0" )
